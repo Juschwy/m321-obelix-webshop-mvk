8d3dd6b1c6f2c2fc77be1ffe80fa2d1bcb989102
Juschwy <jul.fehlmann@gmail.com>
2025-11-03 10:46:35 +0100

    Extract api and dtos into own module

--- obelix-quarry-api/src/main/java/ch/bbw/obelix/quarry/api/QuarryApi.java
+++ obelix-quarry-api/src/main/java/ch/bbw/obelix/quarry/api/QuarryApi.java
@@ -1,5 +1,21 @@
 package ch.bbw.obelix.quarry.api;
 
-public interface QuarryApi {
+import ch.bbw.obelix.quarry.api.dto.MenhirDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.service.annotation.DeleteExchange;
+import org.springframework.web.service.annotation.GetExchange;
 
+import java.util.List;
+import java.util.UUID;
+
+public interface QuarryApi {
+    @GetExchange("/api/quarry")
+    List<MenhirDto> getAllMenhirs();
+
+    @GetExchange("/api/quarry/{menhirId}")
+    MenhirDto getMenhirById(@PathVariable UUID menhirId);
+
+    @DeleteExchange("/api/quarry/{menhirId}")
+    void deleteById(@PathVariable UUID menhirId);
 }
--- obelix-quarry-api/src/main/java/ch/bbw/obelix/quarry/api/dto/BasketDto.java
+++ obelix-quarry-api/src/main/java/ch/bbw/obelix/quarry/api/dto/BasketDto.java
@@ -1,5 +1,6 @@
-package ch.bbw.obelix.webshop.dto;
+package ch.bbw.obelix.quarry.api.dto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import lombok.With;
@@ -8,10 +9,10 @@ import lombok.With;
  * A basket is an offer of various items in exchange for a standing stone.
  */
 @With
-public record BasketDto(List<BasketItem> items) {
+public record BasketDto(ArrayList<BasketItem> items) {
 
 	public static BasketDto empty() {
-		return new BasketDto(List.of());
+		return new BasketDto(new ArrayList<>());
 	}
 
 	public record BasketItem(String name, int count) {
