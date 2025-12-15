dc8564285a60239817fffca87ccab69882ffcdac
Nivre0 <timleu256@gmail.com>
2025-12-15 09:50:53 +0100

    Added create-endpoint for menhir

--- backend/obelix-quarry-impl/src/main/java/ch/bbw/obelix/quarry/controller/QuarryController.java
+++ backend/obelix-quarry-impl/src/main/java/ch/bbw/obelix/quarry/controller/QuarryController.java
@@ -5,14 +5,15 @@ import ch.bbw.obelix.quarry.api.dto.MenhirDto;
 import ch.bbw.obelix.quarry.service.QuarryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.UUID;
 
 /**
- * @author schules
- * @version 27.10.2025
+ * @author Tim Leo Laurin Leuenberger
+ * @version 15.12.2025
  */
 
 @RestController
@@ -33,5 +34,10 @@ public class QuarryController implements QuarryApi {
     @Override
     public MenhirDto getMenhirById(@PathVariable UUID menhirId) {
         return quarryService.getMenhirById(menhirId);
+    }
+
+    @Override
+    public void createMenhir(@RequestBody MenhirDto menhirDto) {
+        quarryService.createMenhir(menhirDto);
     }
 }
