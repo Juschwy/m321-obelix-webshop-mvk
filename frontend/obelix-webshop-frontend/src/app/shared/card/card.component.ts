1b3426c08056f598d11c589fe260b6b48f3f7638
fliberto <fabio.liberto@ergon.ch>
2026-01-05 09:29:24 +0100

    Fix error

--- frontend/obelix-webshop-frontend/src/app/shared/card/card.component.ts
+++ frontend/obelix-webshop-frontend/src/app/shared/card/card.component.ts
@@ -2,11 +2,10 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-card',
-  imports: [],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
+  standalone: true
 })
 export class CardComponent {
-
 
 }
