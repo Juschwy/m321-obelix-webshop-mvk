e93697c2362d2263f7dbc19072cea29b58391108
Juschwy <jul.fehlmann@gmail.com>
2026-01-11 14:26:24 +0100

    more talkative

--- .github/actions/prepareVm/common.sh
+++ .github/actions/prepareVm/common.sh
@@ -18,7 +18,7 @@ login() {
 
   curl -k -c "$COOKIES_FILE" -X POST "$MAAS_URL" \
     --data-raw "login=$user&passwd=$password"
-  echo "$COOKIES_FILE" # TODO: remove
+  cat $COOKIES_FILE
 #  while IFS= read -r line; do
 #    echo "::add-mask::$line"
 #  done < "$COOKIES_FILE"
@@ -28,6 +28,7 @@ login() {
 fetch_vm() {
   local label="$1"
   local json
+  echo "test1"
   json=$(curl -sk -b -v "$COOKIES_FILE" "$MAAS_URL?list")
   echo "json=$json"
   echo "$json" | jq -c --arg label "$label" '.[] | select(.label == $label)'
