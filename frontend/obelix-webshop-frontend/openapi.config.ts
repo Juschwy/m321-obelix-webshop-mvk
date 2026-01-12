80e57f47f5fd65f3ef590e2d220ca62fe502e643
Jan Sigrist <jan.sigrist@ergon.ch>
2026-01-12 10:56:25 +0100

    Remove commented-out legacy swagger configuration in openapi.config.ts.

--- frontend/obelix-webshop-frontend/openapi.config.ts
+++ frontend/obelix-webshop-frontend/openapi.config.ts
@@ -1,13 +1,9 @@
 import {GeneratorConfig} from 'ng-openapi';
 
 export const config: GeneratorConfig = {
-    input: './swagger.json', // or './swagger.json'
+    input: './swagger.json',
     output: './src/api',
     options: {
         generateServices: true, dateType: 'Date', enumStyle: 'enum', generateEnumBasedOnDescription: true,
     }
 }
-//
-// const environment = process.env['ENV'] || 'development';
-// const swaggerUrl = environment === 'production' ? 'https://api.prod.com/swagger/v1/swagger.json' : 'http://localhost:5000/swagger/v1/swagger.json';
-// export const config: GeneratorConfig = {input: swaggerUrl, output: './src/api'};
