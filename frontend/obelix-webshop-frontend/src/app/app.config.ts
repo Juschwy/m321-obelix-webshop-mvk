421637df95a202b5fdfc622a31edb7359ad23a0f
Jan Sigrist <jan.sigrist@ergon.ch>
2026-01-12 10:55:00 +0100

    Configure HTTP client and default API client in app configuration.

--- frontend/obelix-webshop-frontend/src/app/app.config.ts
+++ frontend/obelix-webshop-frontend/src/app/app.config.ts
@@ -1,11 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { provideDefaultClient } from '../api';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(),
+    provideDefaultClient({ basePath: 'http://localhost:8080' }),
   ],
 };
