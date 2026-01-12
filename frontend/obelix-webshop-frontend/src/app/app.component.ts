bf71db53ff31c714f9d8c3108d687a6370bdb6dc
Fabio Liberto <143290934+FabioLiberto@users.noreply.github.com>
2026-01-12 09:36:14 +0100

    Fe/feature/implement menhir card (#61)
    * Initialize component
    
    * Remove unused scss
    - Add test card with styling
    
    * Fix error
    
    * Add inputs for card
    
    * Add demo values
    
    * Add responsiveness
    
    * Implement PR feedback
    
    * Fix merge conflicts
    
    * Fix merge conflicts

--- frontend/obelix-webshop-frontend/src/app/app.component.ts
+++ frontend/obelix-webshop-frontend/src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {CardComponent} from "./shared/card/card.component";
 import {HeaderComponent} from "../webshop/header/header.component";
 import { CartComponent } from './webshop/cart/cart.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [CardComponent, CartComponent, HeaderComponent],
   templateUrl: './app.component.html',
-  imports: [CartComponent],
-    templateUrl: './app.component.html',
+  // TODO remove if card-call is moved to individual components
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {}
