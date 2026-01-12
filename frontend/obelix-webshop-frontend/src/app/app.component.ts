9a66038ac39f77fc4766d9e131cf22ed99cb0985
cxs1mon <caroline.simon@gmx.ch>
2026-01-12 09:40:35 +0100

    move header

--- frontend/obelix-webshop-frontend/src/app/app.component.ts
+++ frontend/obelix-webshop-frontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {HeaderComponent} from "../webshop/header/header.component";
+import {HeaderComponent} from "./webshop/header/header.component";
 import { CartComponent } from './webshop/cart/cart.component';
 
 @Component({
