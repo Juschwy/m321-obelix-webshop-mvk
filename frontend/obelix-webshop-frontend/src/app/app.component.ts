6a5ef2fd7d11496005c01949b934fc4f4baa796d
cxs1mon <118832795+cxs1mon@users.noreply.github.com>
2026-01-12 09:32:35 +0100

    FE: Add header (#45)
    Bypassed, cause build in cicd pipeline is not done yet

--- frontend/obelix-webshop-frontend/src/app/app.component.ts
+++ frontend/obelix-webshop-frontend/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "../webshop/header/header.component";
 import { CartComponent } from './webshop/cart/cart.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [HeaderComponent],
+  templateUrl: './app.component.html',
   imports: [CartComponent],
     templateUrl: './app.component.html',
 })
