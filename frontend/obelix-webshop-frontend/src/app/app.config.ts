04837cb1b330baab75788850cb40f3e665285e50
Mustafa Sagaaro <sagaaromustafa@gmail.com>
2026-01-19 09:23:51 +0100

    merge conflicts resolved

--- frontend/obelix-webshop-frontend/src/app/app.config.ts
+++ frontend/obelix-webshop-frontend/src/app/app.config.ts
@@ -1,6 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideDefaultClient } from '../api';
 
 import { routes } from './app.routes';
 
@@ -9,5 +11,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    provideDefaultClient({ basePath: `${location.protocol}//${location.hostname}:8080` }),
+    provideStore(),
   ],
 };
